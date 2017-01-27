300a2809c455e98b7d3a5c50a785f30210cac345
Marzena Szopinska <marzena_szopinska@hotmail.com>
2017-01-27 10:33:34 +0000

    made the submit button create a new quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,10 +1,22 @@
 class QuotesController < ApplicationController
   def index
-    # A random quote in our database is stored into the variable called @quote
+    # a random quote in our database is stored into the variable called @quote
     @quote = Quote.order("RANDOM()").first
   end
 
   def new
     @quote = Quote.new
+  end
+  # store our quote fields
+  def create
+    Quote.create(quote_params)
+    # send the user to the root_path
+    redirect_to root_path
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
   end
 end
